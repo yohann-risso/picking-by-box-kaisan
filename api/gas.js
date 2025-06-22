a28706cce82f80e8870da5efe22a0cc9e25d045e
yohann-risso <yohannrisso@gmail.com>
2025-06-22 20:40:14 -0300

    FIX API GAS

--- api/gas.js
+++ api/gas.js
@@ -1,20 +1,24 @@
 export default async function handler(req, res) {
   if (req.method !== "POST") {
-    return res.status(405).json({ status: "error", message: "Método não permitido" });
+    return res
+      .status(405)
+      .json({ status: "error", message: "Método não permitido" });
   }
 
   try {
-    const result = await fetch("https://script.google.com/macros/s/AKfycbwUMPjyOqeMBX3vRSdMmJmtnt9-Dt8MHdPYl1aTS9cLaPZ7CuyNuw_uuEwRX0Speh5h/exec", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(req.body),
-    });
+    const response = await fetch(
+      "https://script.google.com/macros/s/AKfycbwUMPjyOqeMBX3vRSdMmJmtnt9-Dt8MHdPYl1aTS9cLaPZ7CuyNuw_uuEwRX0Speh5h/exec",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(req.body),
+      }
+    );
 
-    const json = await result.json();
-    res.status(200).json(json);
-  } catch (error) {
-    res.status(500).json({ status: "error", message: error.message });
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    console.error("Erro no proxy GAS:", err);
+    res.status(500).json({ status: "error", message: err.message });
   }
 }
