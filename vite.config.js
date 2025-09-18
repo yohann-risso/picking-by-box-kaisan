4e9356541892718abf442b63d39b250763b86cfa
yohann-risso <yohannrisso@gmail.com>
2025-09-18 12:22:09 -0300

    fixes

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 // vite.config.js
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   root: 'src',
@@ -7,8 +8,14 @@ export default defineConfig({
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        admin: resolve(__dirname, 'src/admin.html')
+      }
+    }
   },
   server: {
     port: 3000,
   },
-});
+})
